6be492a9e2a6b5d4998abbc0ad1f176eb6f1013a
amita987 <agiri134@gmail.com>
2026-07-17 11:03:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+// Bite Amit Restaurant Website
 
+console.log("Welcome to Bite Amit Restaurant!");
+
+function orderNow() {
+    alert("Thank you for choosing Bite Amit! We will contact you soon.");
+}
