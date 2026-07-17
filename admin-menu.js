28bb6aa62b0a910e97f4c2eeaa9f76a076fa5235
amita987 <agiri134@gmail.com>
2026-07-17 20:09:33 +0530

    Create admin-menu.js

---
+++ admin-menu.js
@@ -0,0 +1,166 @@
+/* ==========================================================
+   FILE : admin-menu.js
+
+   PURPOSE:
+   Displays restaurant menu for administrator.
+
+   CURRENT FEATURES
+
+   ✓ View Menu
+
+   FUTURE FEATURES
+
+   - Edit Item
+   - Delete Item
+   - Add Item
+
+   ========================================================== */
+
+
+
+/* ==========================================================
+   DISPLAY MENU
+
+   PURPOSE:
+   Reads menuData from menu-data.js
+   and creates an Admin Menu table.
+   ========================================================== */
+
+const adminContainer =
+
+document.getElementById(
+
+    "admin-menu-container"
+
+);
+
+
+
+let table = `
+
+<table border="1" width="100%">
+
+<tr>
+
+<th>Action</th>
+
+<th>S.No</th>
+
+<th>Image</th>
+
+<th>Item</th>
+
+<th>Category</th>
+
+<th>Price</th>
+
+<th>Discount</th>
+
+<th>Final Price</th>
+
+</tr>
+
+`;
+
+
+
+menuData.forEach(function(category){
+
+
+    category.items.forEach(function(item){
+
+
+        let finalPrice =
+
+        item.price -
+
+        (item.price * item.discount / 100);
+
+
+
+        table += `
+
+<tr>
+
+<td>
+
+✏️
+
+🗑️
+
+</td>
+
+
+<td>
+
+${item.id}
+
+</td>
+
+
+<td>
+
+<img
+
+src="${item.image}"
+
+width="60">
+
+</td>
+
+
+<td>
+
+${item.name}
+
+</td>
+
+
+<td>
+
+${category.category}
+
+</td>
+
+
+<td>
+
+₹${item.price}
+
+</td>
+
+
+<td>
+
+${item.discount}%
+
+</td>
+
+
+<td>
+
+₹${finalPrice}
+
+</td>
+
+</tr>
+
+`;
+
+
+    });
+
+
+});
+
+
+
+table +=
+
+"</table>";
+
+
+
+adminContainer.innerHTML =
+
+table;
