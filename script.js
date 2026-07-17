8f181d5e8da48d293014fdd2bfc05f9b82373ac5
amita987 <agiri134@gmail.com>
2026-07-17 14:37:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,75 @@
-// Bite Amit Restaurant Website
+const menuContainer = document.getElementById("menu-container");
 
-console.log("Welcome to Bite Amit Restaurant!");
+menuData.forEach(category => {
 
-function orderNow() {
-    alert("Thank you for choosing Bite Amit! We will contact you soon.");
-}
+    let html = `
+        <h2>${category.category}</h2>
+
+        <table border="1" cellpadding="10" cellspacing="0" width="100%">
+
+            <tr>
+                <th>S.No</th>
+                <th>Image</th>
+                <th>Item</th>
+                <th>Price</th>
+                <th>Discount</th>
+                <th>Final Price</th>
+                <th>Quantity</th>
+                <th>Add to Cart</th>
+            </tr>
+    `;
+
+    category.items.forEach(item => {
+
+        const finalPrice =
+            item.price - (item.price * item.discount / 100);
+
+        html += `
+
+        <tr>
+
+            <td>${item.id}</td>
+
+            <td>
+                <img src="${item.image}"
+                     width="80">
+            </td>
+
+            <td>${item.name}</td>
+
+            <td>₹${item.price}</td>
+
+            <td>${item.discount}% OFF</td>
+
+            <td>₹${finalPrice}</td>
+
+            <td>
+
+                <button>-</button>
+
+                1
+
+                <button>+</button>
+
+            </td>
+
+            <td>
+
+                <button>Add to Cart</button>
+
+            </td>
+
+        </tr>
+
+        `;
+
+    });
+
+    html += "</table><br><br>";
+
+    menuContainer.innerHTML += html;
+
+});
+
+
+
