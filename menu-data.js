1bb4f22479403fd297fa92aaf74a7cb5dbfa38d1
amita987 <agiri134@gmail.com>
2026-07-17 14:34:18 +0530

    Update menu-data.js

--- menu-data.js
+++ menu-data.js
@@ -1,1 +1,72 @@
+const menuData = [
 
+    {
+        category: "Fast Food",
+        items: [
+
+            {
+                id: 1,
+                name: "Veg Burger",
+                image: "images/burger.jpg",
+                price: 180,
+                discount: 10
+            },
+
+            {
+                id: 2,
+                name: "French Fries",
+                image: "images/fries.jpg",
+                price: 120,
+                discount: 5
+            }
+
+        ]
+    },
+
+    {
+        category: "Main Course",
+        items: [
+
+            {
+                id: 1,
+                name: "Paneer Butter Masala",
+                image: "images/paneer.jpg",
+                price: 320,
+                discount: 15
+            },
+
+            {
+                id: 2,
+                name: "Veg Biryani",
+                image: "images/biryani.jpg",
+                price: 280,
+                discount: 10
+            }
+
+        ]
+    },
+
+    {
+        category: "Drinks",
+        items: [
+
+            {
+                id: 1,
+                name: "Mango Shake",
+                image: "images/mango-shake.jpg",
+                price: 160,
+                discount: 10
+            },
+
+            {
+                id: 2,
+                name: "Cold Coffee",
+                image: "images/cold-coffee.jpg",
+                price: 180,
+                discount: 5
+            }
+
+        ]
+    }
+
+];
