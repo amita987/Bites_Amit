c84e9ccc7ad8ad72f4ab5add1e66d7e23e585fe1
amita987 <agiri134@gmail.com>
2026-07-17 16:02:06 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -63,8 +63,43 @@ function addToCart(button){
    
    }
    
+
+   /* ==========================================================
+      CART COUNTER
+   
+      Updates the number displayed beside the Cart menu.
+      ========================================================== */
+   
+   updateCartCount();
+   
    console.log(cart);
    
    alert(itemName + " added to cart.");
 
 }
+
+
+/* ==========================================================
+   UPDATE CART COUNTER
+
+   Displays the total number of unique items in the cart.
+   ========================================================== */
+
+function updateCartCount(){
+
+    const cartCount =
+        document.getElementById("cart-count");
+
+    if(cartCount){
+
+        cartCount.textContent = "(" + cart.length + ")";
+
+    }
+
+}
+
+
+
+
+
+
