78a16cb6eeb1b29a7bf3433802d5a47468d2ddf8
amita987 <agiri134@gmail.com>
2026-07-17 15:35:41 +0530

    Create cart.js

---
+++ cart.js
@@ -0,0 +1,21 @@
+/* ==========================================================
+   SHOPPING CART
+
+   Stores items selected by the customer.
+   ========================================================== */
+
+let cart = [];
+
+
+
+/* ==========================================================
+   ADD TO CART
+
+   Adds a selected menu item into the shopping cart.
+   ========================================================== */
+
+function addToCart(button){
+
+    alert("Shopping cart connected successfully.");
+
+}
