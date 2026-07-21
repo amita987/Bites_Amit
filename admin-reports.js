6128f5d4e9a07e3f77c96a1d418fa315d4bba453
amita987 <agiri134@gmail.com>
2026-07-21 21:28:55 +0530

    Create admin-reports.js

---
+++ admin-reports.js
@@ -0,0 +1,30 @@
+/* =====================================================
+   Reports & Dashboard
+
+   This file contains all report calculations.
+
+   Reports are ALWAYS calculated from:
+
+   1. restaurantOrders
+   2. purchaseRegister
+
+   Nothing is stored separately.
+===================================================== */
+
+
+
+/* =====================================================
+   Page Loaded
+===================================================== */
+
+document.addEventListener(
+
+    "DOMContentLoaded",
+
+    function () {
+
+        console.log("Reports page loaded.");
+
+    }
+
+);
