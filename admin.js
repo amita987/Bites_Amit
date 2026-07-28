152d431062ebbbc9a12a6e0662ffa1db17aa3f02
amita987 <agiri134@gmail.com>
2026-07-28 22:07:55 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -227,7 +227,40 @@ function forgotPassword(){
 
 }
 
+/* ==========================================================
+   SHOW / HIDE PASSWORD
 
+   PURPOSE:
+   Toggles password visibility.
+========================================================== */
+
+
+function togglePassword(){
+
+
+    let passwordField =
+
+        document.getElementById("password");
+
+
+    if(passwordField.type === "password"){
+
+
+        passwordField.type = "text";
+
+
+    }
+
+    else{
+
+
+        passwordField.type = "password";
+
+
+    }
+
+
+}
 
 
 
